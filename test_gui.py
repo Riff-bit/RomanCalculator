43bfaaa37f85ef3e6ee88c745b773dc28411fc0e
Leon Diercks <leon.diercks@t-online.de>
2019-09-21 11:39:41 +0200

    :sparkles: working converter gui

--- test_gui.py
+++ test_gui.py
@@ -1,17 +1,27 @@
 from tkinter import *
 from tkinter import ttk
+import roman
+
+def umwandeln():
+    arab = arabicVar.get()
+    print(arab, type(arab))
+    romanVar.set(roman.fromArabic(int(arab)))
 
 root = Tk()
+
+arabicVar = StringVar()
+romanVar = StringVar()
 
 mainframe = ttk.Frame(root, padding="3 3 12 12")
 mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
 
-w = Label(root, text="Hello Tkinter!").grid(row = 0)
+w = Label(root, textvariable=romanVar).grid(row=0)
 
+e1 = Entry(root, textvariable=arabicVar)
+e1.grid(row=0, column=1)
+e1.focus_set()
 
-e1 = Entry(root)
-
-e1.grid(row = 0, column = 1)
-
+b = Button(root, text="umwandeln", width=10, command=umwandeln)
+b.grid(row=2)
 
 root.mainloop()
