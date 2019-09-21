0ddf2f8b2ac9e39961e40448a9143b9cef42eb26
Luca Sundermeier <luca.sundermeier@web.de>
2019-09-21 21:06:44 +0200

    :sparkles: general improvement of UX

--- test_luca.py
+++ test_luca.py
@@ -2,33 +2,35 @@ from tkinter import *
 from tkinter import ttk
 import roman
 
-
+def delete():
+    inputVar.set("")
+    outputVar.set("")
 
 def umwandeln():
-    arab = arabicVar.get()
+    arab = inputVar.get()
     direction = tkvar.get()
     if direction == choices[0]:
-        romanVar.set(roman.fromArabic(int(arab)))
+        outputVar.set(roman.fromArabic(int(arab)))
     elif direction == choices[1]:
         result = roman.toArabic(arab)
         if result > 0:
-            romanVar.set(result)
+            outputVar.set(result)
         else:
-            romanVar.set("Immutabilis!")
+            outputVar.set("Immutabilis!")
 
 root = Tk()
 root.title("Umrechner Römisch / Arabisch")
 
-arabicVar = StringVar()
-romanVar = StringVar()
+inputVar = StringVar()
+outputVar = StringVar()
 
 mainframe = ttk.Frame(root, padding="3 3 12 12")
 mainframe.grid(column=9, row=7, sticky=(N, W, E, S))
 
-w = Label(root, textvariable=romanVar).grid(row=2, column=3, sticky = W)
+w = Label(root, textvariable=outputVar).grid(row=2, column=3, sticky = W)
 root.geometry("500x500")
 
-e1 = Entry(root, textvariable=arabicVar)
+e1 = Entry(root, textvariable=inputVar)
 e1.grid(row=1, column=3)
 e1.focus_set()
 
@@ -55,4 +57,13 @@ lableausgabe.grid(row=2, column=2)
 
 lableleer = Label(root, text="      ")
 lableleer.grid(row=0)
+
+lableleer2 = Label(root, text="      ")
+lableleer2.grid(row=4, column=4)
+
+bdelete = Button(root, text="Kapitulation!", width=10, command=delete)
+bdelete.grid(row=3, column=3, sticky=E)
+
+
 root.mainloop()
+
